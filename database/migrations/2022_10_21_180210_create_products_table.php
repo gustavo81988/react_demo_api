de100b85c5ee851a60223353b6c95763726948e0
Gustavo Ramirez <gramirez@overall.com.pe>
2022-10-21 14:13:43 -0400

    migrations for: categories, products

---
+++ database/migrations/2022_10_21_180210_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->index('id_category');
+            $table->string('name',100)->unique();
+            $table->string('description',300);
+            $table->double('price', 8, 2);
+            $table->bigInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
